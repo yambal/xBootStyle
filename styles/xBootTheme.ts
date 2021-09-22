5cf78fa828eb08945bab2524331e2fea8dfe4633
June YAMAMOTO <june@yambal.net>
2021-09-22 23:44:03 +0900

    Fix

--- styles/xBootTheme.ts
+++ styles/xBootTheme.ts
@@ -1,10 +1,12 @@
 import { darken } from "polished"
+import { defaultTheme } from "@xstyled/styled-components"
 
 /**
  * WIP : ThemeProvider (@xstyled/styled-components)向けテーマ
  **/
 
 export const xBootTheme = {
+  ...defaultTheme,
   fonts: {
     sansSerif: 'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
     monospace: 'SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;'
